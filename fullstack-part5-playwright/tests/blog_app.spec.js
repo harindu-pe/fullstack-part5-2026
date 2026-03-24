0d9a546b9d8523c90964226fff2e9b1b1338ce93
harindu-pe <harindu.pe@gmail.com>
2026-03-24 19:31:40 +0530

    exercise 5.18 done

--- fullstack-part5-playwright/tests/blog_app.spec.js
+++ fullstack-part5-playwright/tests/blog_app.spec.js
@@ -1,7 +1,18 @@
 const { test, describe, expect, beforeEach } = require("@playwright/test");
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    // empty the db here
+    await request.post("http://localhost:3003/api/testing/reset");
+    // create a user for the backend here
+    await request.post("http://localhost:3003/api/users", {
+      data: {
+        name: "Matti Luukkainen",
+        username: "mluukkai",
+        password: "salainen",
+      },
+    });
+    // go to page
     await page.goto("http://localhost:5173");
   });
 
@@ -11,5 +22,25 @@ describe("Blog app", () => {
 
     const locator2 = page.getByRole("button", { name: "login" });
     await expect(locator2).toBeVisible();
+  });
+
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await page.getByRole("textbox").first().fill("mluukkai");
+      await page.getByRole("textbox").last().fill("salainen");
+      await page.getByRole("button", { name: "login" }).click();
+
+      await expect(page.getByText("mluukkai logged in")).toBeVisible();
+    });
+
+    test("fails with wrong credentials", async ({ page }) => {
+      await page.getByRole("textbox").first().fill("mluukkai");
+      await page.getByRole("textbox").last().fill("wrong");
+      await page.getByRole("button", { name: "login" }).click();
+
+      await expect(
+        page.getByText("invalid username or password"),
+      ).toBeVisible();
+    });
   });
 });
